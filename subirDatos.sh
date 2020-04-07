dc86fb9289afffb30fa2405c88f26d82f0010607
John Sanabria <john.sanabria@gmail.com>
2020-04-07 20:10:37 +0000

    making script aware about where it is located

--- subirDatos.sh
+++ subirDatos.sh
@@ -5,10 +5,18 @@
 # encuentra definida en 'config.cfg'. Sin embargo, el archivo 'config.cfg' 
 # solo se instanciara al momento de desplear los scripts.
 #
+#
 # AUTHOR: John Sanabria - john.sanabria@correounivalle.edu.co
 # DATE: 2020-04-07
 #
-. config.cfg
+BASEPATH=$(dirname "$(realpath ${0})") 
+CWD=$(pwd)
+cd ${BASEPATH}
+if [ -f config.cfg ]; then
+  . config.cfg
+fi
 MEM=$(./freeMem.sh)
-curl https://api.thingspeak.com/update?api_key=${TSWRITEKEY}\&field1=${MEM}
+#curl https://api.thingspeak.com/update?api_key=${TSWRITEKEY}\&field1=${MEM}
+curl https://api.thingspeak.com/update?api_key=0PE2VXR5ZZB60N0A\&field1=${MEM}
+cd ${CWD}
 
